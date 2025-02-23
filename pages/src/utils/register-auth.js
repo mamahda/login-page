a389d61dbee59345d8915cbc4482f58682f72321
mamahda <gbrnmhd89@gmail.com>
2025-02-23 23:03:59 +0700

    updates

--- pages/src/utils/register-auth.js
+++ pages/src/utils/register-auth.js
@@ -1,27 +1,27 @@
 export async function registerAuth(username, email, password, setMessage) {
 
   const endpoint = "http://localhost:3002/api/users/register";
-  
+
   try {
-      const response = await fetch(endpoint, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username, email, password }),
-      });
+    const response = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, email, password }),
+    });
 
-      const data = await response.json();
-      setMessage(data.message);
+    const data = await response.json();
+    setMessage(data.message);
 
-      if (data.success) {
-        return true;
-      }
+    if (data.success) {
+      return true;
+    }
     else {
       return false;
     }
-    } catch (error) {
-      console.error("Error registering user:", error);
-      return false;
-    }
-  };
+  } catch (error) {
+    console.error("Error registering user:", error);
+    return false;
+  }
+};
